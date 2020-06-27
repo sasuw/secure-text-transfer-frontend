c0c7c337aa55a57b4284039774e4d88749dc66f5
Sasu <git@sasu.net>
2020-06-27 16:51:56 +0200

    First basically working version

--- js/main.js
+++ js/main.js
@@ -3,139 +3,153 @@ Globals = {
     inputBorderColor: null
 };
 
-function main(){
+function log(string){
+    console.log(string);
+}
+
+function main() {
+    log('main started');
     let ihavepin = document.getElementById('ihavepin');
     let ihavepwd = document.getElementById('ihavepwd');
-    
+
     //addEventListeners(ihavepin, ihavepwd);
     //addEventListeners(ihavepwd, ihavepin);
 
-    function addEventListeners(sourceEl, targetEl){
-        sourceEl.addEventListener("mouseover", function( event ) {   
+    function addEventListeners(sourceEl, targetEl) {
+        sourceEl.addEventListener("mouseover", function (event) {
             targetEl.style.opacity = 0.4;
         });
-        sourceEl.addEventListener("mouseout", function( event ) {   
+        sourceEl.addEventListener("mouseout", function (event) {
             targetEl.style.opacity = 1.0;
         });
     }
-    
+
     document.getElementById('time').innerText = getInitialTimeString();
-    
+
     let pwdInput = document.getElementById('pwdInput');
     Globals.inputBorderColor = pwdInput.style.borderColor;
 }
 
-function show(elId){
+function show(elId) {
     document.getElementById(elId).style.display = 'block';
 }
 
-function hide(elId){
+function hide(elId) {
     document.getElementById(elId).style.display = 'none';
 }
 
-function toggleDisplay(el){
-    if(el.style.display === 'block' || el.style.display === ''){
-        el.style.display = 'none';
-    }else if(el.style.display === 'none'){
-        el.style.display = 'block';
-    }
-    
-    let pinValueEl = document.getElementById('pinValue');
-    let pinValue = getPinForString();
-    pinValue.innerText = pinValue;
-}
-
-function showPinForString(string){
+function showPinForString(string) {
     let pwdInput = document.getElementById('pwdInput');
-    if(pwdInput.value.length === 0){
+    if (pwdInput.value.length === 0) {
         pwdInput.style.borderColor = '#ff0000';
         return;
     }
-    
+
     hide('ihavepwd');
     show('igotpin');
-    
-    startTimer(getInitialTimeInSeconds(), document.getElementById('time'));
+
+    startTimer(getInitialTimeInSeconds(), document.getElementById('time'), pinTimeExpired);
 }
 
-function showPwdForPin(pin){
+function showPwdForPin(pin) {
     let pinInput = document.getElementById('pinInput');
-    if(pinInput.value.length === 0){
+    if (pinInput.value.length === 0) {
         pinInput.style.borderColor = '#ff0000';
         return;
     }
-    
-    let ihavepwd = document.getElementById('ihavepwd');
-    let igotpin = document.getElementById('igotpin');
-    toggleDisplay(ihavepwd);
-    toggleDisplay(igotpin);
+
+    hide('ihavepin');
+    show('igotpwd');
+
+    startTimer(getInitialTimeInSeconds(), document.getElementById('time2'), pwdTimeExpired);
 }
 
-function focusField(field){
+function focusField(field) {
     field.style.borderColor = Globals.inputBorderColor;
 }
 
-function getPinForString(){
+function getPinForString() {
     return '1234';
 }
 
-function getInitialTimeInSeconds(){
-    return 10;
+function getInitialTimeInSeconds() {
+    return 6;
 }
 
-function getInitialTimeString(){
+function getInitialTimeString() {
     let time = getInitialTimeInSeconds();
     let minutes = Math.floor(time / 60);
     let seconds = time % 60;
-    
+
     let minutesString = minutes.toString();
     let secondsString = seconds.toString();
-    
-    if(minutesString.length === 1){
+
+    if (minutesString.length === 1) {
         minutesString = '0' + minutesString;
     }
-    if(secondsString.length === 1){
+    if (secondsString.length === 1) {
         secondsString = '0' + secondsString;
     }
-    
+
     return minutesString + ':' + secondsString;
 }
-function timeExpired(){
+
+function pwdTimeExpired(){
+    document.getElementById('stringStart').innerText = 'Your string has expired';
+    document.getElementById('pwdValue').innerText = '';
+    hide('pwdHint');
+    clearInterval(Globals.intervalId);
+    document.getElementById('gotitPwd').value = 'Start over';
+}
+
+function pinTimeExpired() {
     hide('pinHint');
     show('timeExpired');
+
+    log('clearing timer');
     clearInterval(Globals.intervalId);
-    
-    document.getElementById('gotit').value = 'Start over';
+
+    document.getElementById('gotitPin').value = 'Start over';
 }
 
-function gotIt(){
-    let ihavepwd = document.getElementById('ihavepwd');
-    let igotpin = document.getElementById('igotpin');
-    let pinHint = document.getElementById('pinHint');
-    let timeExpired = document.getElementById('timeExpired');
+function gotItPin() {
+    document.getElementById('pwdInput').value = '';
 
-    toggleDisplay(pinHint);
-    toggleDisplay(timeExpired);
-    toggleDisplay(ihavepwd);
-    toggleDisplay(igotpin);
-    
+    show('ihavepwd');
+    hide('igotpin');
+
+    log('clearing timer');
     clearInterval(Globals.intervalId);
 }
 
-function startTimer(duration, display) {
+function gotItPwd() {
+    document.getElementById('pinInput').value = '';
+
+    show('ihavepin');
+    hide('igotpwd');
+
+    log('clearing timer');
+    clearInterval(Globals.intervalId);
+}
+
+function startTimer(duration, display, timerEndFunction) {
     var timer = duration, minutes, seconds;
-    
+
     let timerFunction = function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+        try {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+            display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timeExpired();
+            if (--timer < 0) {
+                timerEndFunction();
+            }
+        } catch (error) {
+            console.log('error: ' + error);
         }
     };
     timerFunction();
