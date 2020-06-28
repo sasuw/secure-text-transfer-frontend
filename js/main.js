9512f41828ef54d6a550e49c4fcd5ffe97b45892
Sasu <git@sasu.net>
2020-06-28 13:19:34 +0200

    Add backend integration

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ Globals = {
     inputBorderColor: null
 };
 
-function log(string){
+function log(string) {
     console.log(string);
 }
 
@@ -38,17 +38,29 @@ function hide(elId) {
     document.getElementById(elId).style.display = 'none';
 }
 
-function showPinForString(string) {
-    let pwdInput = document.getElementById('pwdInput');
-    if (pwdInput.value.length === 0) {
-        pwdInput.style.borderColor = '#ff0000';
-        return;
+function showPinForString() {
+    try {
+        let pwdInput = document.getElementById('pwdInput');
+        if (pwdInput.value.length === 0) {
+            pwdInput.style.borderColor = '#ff0000';
+            return;
+        }
+
+        hide('ihavepwd');
+        show('igotpin');
+
+        startTimer(getInitialTimeInSeconds(), document.getElementById('time'), pinTimeExpired);
+
+        getPinForString(pwdInput.value).then(data => {
+            try {
+                document.getElementById('pinValue').innerText = data;
+            } catch (error) {
+                log('Error 2: ' + error);
+            }
+        });
+    } catch (error) {
+        log('Error: ' + error);
     }
-
-    hide('ihavepwd');
-    show('igotpin');
-
-    startTimer(getInitialTimeInSeconds(), document.getElementById('time'), pinTimeExpired);
 }
 
 function showPwdForPin(pin) {
@@ -62,18 +74,56 @@ function showPwdForPin(pin) {
     show('igotpwd');
 
     startTimer(getInitialTimeInSeconds(), document.getElementById('time2'), pwdTimeExpired);
+
+    getStringForPin(pinInput.value).then(data => {
+        try {
+            document.getElementById('pwdValue').innerText = data;
+        } catch (error) {
+            log('Error 2: ' + error);
+        }
+    });
 }
 
 function focusField(field) {
     field.style.borderColor = Globals.inputBorderColor;
 }
 
-function getPinForString() {
-    return '1234';
+async function getPinForString(string) {
+    let url = 'http://127.0.0.1:10000/string'
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: string // body data type must match "Content-Type" header
+    });
+    return response.text(); // parses JSON response into native JavaScript objects
+}
+
+async function getStringForPin(string) {
+    let url = 'http://127.0.0.1:10000/pin'
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: string // body data type must match "Content-Type" header
+    });
+    return response.text(); // parses JSON response into native JavaScript objects
 }
 
 function getInitialTimeInSeconds() {
-    return 6;
+    return 667;
 }
 
 function getInitialTimeString() {
@@ -94,7 +144,7 @@ function getInitialTimeString() {
     return minutesString + ':' + secondsString;
 }
 
-function pwdTimeExpired(){
+function pwdTimeExpired() {
     document.getElementById('stringStart').innerText = 'Your string has expired';
     document.getElementById('pwdValue').innerText = '';
     hide('pwdHint');
